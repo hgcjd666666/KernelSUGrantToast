7d1d07fe873e4ae01f9406fef5d1e8dccb5867c3
NativeStar <z2858470935@163.com>
2026-06-11 17:09:33 +0800

    webUi接入gradle构建

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.android.build.gradle.internal.services.computeMaxAapt2Daemons
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.Delete
 import org.gradle.api.tasks.bundling.Zip
@@ -7,11 +8,25 @@ plugins {
 }
 val exportRootDir = file("./tempOutput")
 val stagedModuleDir = exportRootDir.resolve("KernelSUGrantToast")
-val apkOutput = project(":app").layout.buildDirectory.file("outputs/apk/release/app-release-unsigned.apk")
+val apkOutput =
+    project(":app").layout.buildDirectory.file("outputs/apk/release/app-release-unsigned.apk")
 val apkNameAfterRename = "daemon.apk"
 val zipName = "KernelSUGrantToast.zip"
+val buildWeb by tasks.registering(Exec::class) {
+    workingDir = rootProject.file("web")
+    commandLine(
+        if (System.getProperty("os.name")
+                .startsWith("Windows", ignoreCase = true)
+        ) "npm.cmd" else "npm",
+        "run", "build"
+    )
+}
 val cleanPackWorkspace by tasks.registering(Delete::class) {
     delete(stagedModuleDir, exportRootDir.resolve(zipName))
+}
+buildWeb.configure {
+    // web 输出目录位于 stagedModuleDir 下，避免产物被清理任务删掉
+    mustRunAfter(cleanPackWorkspace)
 }
 val copyModule by tasks.registering(Copy::class) {
     dependsOn(cleanPackWorkspace)
@@ -40,7 +55,7 @@ val copyRenamedApk by tasks.registering(Copy::class) {
     }
 }
 val zipCopiedModule by tasks.registering(Zip::class) {
-    dependsOn(copyRenamedApk)
+    dependsOn(copyRenamedApk,buildWeb)
     from(stagedModuleDir)
     destinationDirectory.set(exportRootDir)
     archiveFileName.set(zipName)
